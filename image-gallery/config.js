62a434232d3bc0917e3827bc79aa4746a3928f9d
Baptiste <36029119+Etistab@users.noreply.github.com>
2021-02-10 20:00:09 +0100

    Add files via upload

---
+++ image-gallery/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  s3: {
+    accessKeyId: '64d049eb672a4bc9b4b46912d8bae0e6', // TODO
+    secretAccessKey: 'a1de9a09bad804431a4bf2918b6c780124323cc50cdaf52b', // TODO
+    endpoint: 'https://control.cloud-object-storage.cloud.ibm.com/v2/endpoints', // TODO
+    bucket: 'esgi-cloud-infra-files-s7efu-bucket', // TODO
+  },
+  suffix: {
+    small: '_small.jpg',
+    full: '_full.jpg'
+  }
+};
