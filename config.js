565a1b2baa2503b7bfc1c3ddab47ce69046bd2f1
Baptiste <baptiste.lego1@gmail.com>
2021-02-10 20:12:04 +0100

    update bucket name

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
     accessKeyId: '64d049eb672a4bc9b4b46912d8bae0e6', // TODO
     secretAccessKey: 'a1de9a09bad804431a4bf2918b6c780124323cc50cdaf52b', // TODO
     endpoint: 'https://control.cloud-object-storage.cloud.ibm.com/v2/endpoints', // TODO
-    bucket: 'esgi-cloud-infra-files-s7efu-bucket', // TODO
+    bucket: 'crn:v1:bluemix:public:cloud-object-storage:global:a/ccfc4d72b9bb436d9eb50f7cc545bc55:a0c3b95d-0e3a-49e5-8973-c3aef377b581::', // TODO
   },
   suffix: {
     small: '_small.jpg',
